0f355bc06622694ff646a29905394a486222cd0d
Boseong Kang <rkdqhtjd77@naver.com>
2026-03-29 19:37:21 +0900

    fix: dotenv수정 (로컬)

--- src/main/java/com/capstone/arfly/ArfLyApplication.java
+++ src/main/java/com/capstone/arfly/ArfLyApplication.java
@@ -9,7 +9,11 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class ArfLyApplication {
 
     public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.load();
+        // 파일이 없어도 에러를 내지 않고 무시하도록 설정 (로컬 개발 .env를 위해서)
+        Dotenv dotenv = Dotenv.configure()
+                .ignoreIfMissing()
+                .load();
+
         dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 
         SpringApplication.run(ArfLyApplication.class, args);
