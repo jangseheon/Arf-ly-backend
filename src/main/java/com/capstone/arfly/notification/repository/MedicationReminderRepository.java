f5db0198f5378697387592dbed0746a0814ef0a0
White_DeepDive <tlgud119@yu.ac.kr>
2026-04-08 15:02:20 +0900

    refactor: 알람 발송 조건 수정
    - 과거 시간 알람 즉시 발송 방지: 현재 시간보다 이전으로 설정된 알람이 등록 즉시 울리는 현상을 해결
    - 알림 시간 수정 대응: 오늘 이미 알람이 울렸더라도, 발송 이후에 시간을 수정 했다면 새로운 시간에 다시울리도록 개선
    - 알람 안정성 확보: <= 연산자를 유지하여 서버 지연으로 인한 미발송 알람을 놓치지 않도록 설계

--- src/main/java/com/capstone/arfly/notification/repository/MedicationReminderRepository.java
+++ src/main/java/com/capstone/arfly/notification/repository/MedicationReminderRepository.java
@@ -17,23 +17,27 @@ public interface MedicationReminderRepository extends JpaRepository<MedicationRe
     //알람 대상자 찾기
     @Query("""
                 SELECT new com.capstone.arfly.common.dto.MedicationAlarmDto(
-                    m.id, m.title, COALESCE(m.content,"알람이 울립니다!"), m.reminderTime, t.id, t.token, t.deviceType
+                    m.id, m.title, COALESCE(m.content, '약 복용 시간입니다!'), m.reminderTime, t.id, t.token, t.deviceType
                 )
                 FROM MedicationReminder m\s
                 JOIN m.member mem
                 JOIN FcmToken t ON mem = t.member
                 WHERE m.reminderTime <= :currentTime\s
-                AND m.active = true\s
-                AND mem.notificationEnabled = true
-                AND (
-                    m.lastSentAt IS NULL OR m.lastSentAt < :startOfToday OR\s
-                    (m.lastSentAt < m.timeUpdatedAt AND m.reminderTime >= CAST(m.timeUpdatedAt AS localtime ) ))
-           \s""")// 아직 한 번도 보낸 적 없거나, 마지막으로 보낸 게 어제 이전이거나, 오늘 보냈어도, 그 이후에 시간을 수정한 경우
+                  AND m.active = true\s
+                  AND mem.notificationEnabled = true
+                  AND (
+                      ((m.lastSentAt IS NULL OR m.lastSentAt < :startOfToday)\s
+                        AND m.reminderTime >= CAST(m.timeUpdatedAt AS localtime))
+                      OR\s
+                      (m.lastSentAt < m.timeUpdatedAt\s
+                        AND m.reminderTime >= CAST(m.timeUpdatedAt AS localtime))
+                  )
+            """)//Case 1: 10시에 설정했는데 8시에 울려야하는 경우, Case 2: 10시에 설정했는데 10시 10분에  울려야하는 경우
+    //Case 3: 3. 10시에 설정해서 울렸는데 시간 변경으로 11시로해서 또 울려야하는 경우
     List<MedicationAlarmDto> findPendingNotifications(
             @Param("currentTime") LocalTime currentTime,
             @Param("startOfToday") LocalDateTime startOfToday
     );
-
     //푸시 알람 전송 시간 업데이트
     @Modifying(clearAutomatically = true)
     @Query("UPDATE MedicationReminder m SET m.lastSentAt = :now WHERE m.id IN :ids")
