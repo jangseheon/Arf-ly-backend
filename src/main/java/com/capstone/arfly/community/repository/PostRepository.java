3169b27755136939904ca6c96f7dd658fee35355
White_DeepDive <tlgud119@yu.ac.kr>
2026-04-29 13:27:46 +0900

    fix(Post): getPostDetail 함수에서 발생하는 N+1 문제 해결
    - 문제: findById 함수 호출 후  post.getMember().getNickName() 호출 시 SELECT Query 추가로 발생
    - 해결: EntityGraph를 findById 함수에 명시하여 조건에 맞는 게시물 조회 시 Member Entity도 함께 로딩하여 해결

--- src/main/java/com/capstone/arfly/community/repository/PostRepository.java
+++ src/main/java/com/capstone/arfly/community/repository/PostRepository.java
@@ -2,10 +2,12 @@ package com.capstone.arfly.community.repository;
 
 import com.capstone.arfly.community.domain.Post;
 import java.util.Optional;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post,Long> {
+    @EntityGraph(attributePaths = {"member"})
     Optional<Post>findById(Long id);
 }
