ae3a3e7025f18542a73dd7a7493ca2affa89d27e
White_DeepDive <tlgud119@yu.ac.kr>
2026-04-29 13:21:04 +0900

    fix(Pet): getPetDetail 함수에서 발생하는 N+1 문제 해결
    - 문제: finById 호출 시 조건에 맞는 Pet만 조회하고 Member, breeds 등은 조회하지 않기 때문에 접근 시 SELECT 쿼리 추가 발생(+3)
    - 해결: EntityGraph 어노테이션을 findById 함수에 적용함으로써 단일 쿼리에 연관 엔티티 함꼐 로딩 -> 1번의 SELECT만 발생

--- src/main/java/com/capstone/arfly/pet/repository/PetRepository.java
+++ src/main/java/com/capstone/arfly/pet/repository/PetRepository.java
@@ -1,6 +1,8 @@
 package com.capstone.arfly.pet.repository;
 
 import com.capstone.arfly.pet.domain.Pet;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -15,5 +17,8 @@ public interface PetRepository extends JpaRepository<Pet, Long> {
     SELECT p FROM Pet  p LEFT JOIN FETCH p.profileImage WHERE p.member.id = :memberId 
     """)
     List<Pet> findAllByMemberId(@Param("memberId") Long memberId);
+
+    @EntityGraph(attributePaths = {"member","breeds","profileImage"})
+    Optional<Pet>findById(long petId);
 
 }
