7ff5816ca1bfd823121a050dab9d5ecec1d7f27e
White_DeepDive <tlgud119@yu.ac.kr>
2026-04-29 14:59:52 +0900

    refactor(Post) LikeSyncScheduler의 좋아요 DB 동기화를  batch 방식으로 성능 개선
    - 기존 : Redis scan 후 1개씩 update 쿼리 실행으로 N번의 DB 호출 발생
    - 변경: post Id와 count를 Map에 저장하고 jdbcTemplate의 batchUpdate 함수를 호출하여 한번에 일괄 처리
    - 이를 통해 DB 커넥션 횟수를 1번으로 감소시키고 배치 처리로 동기화 성능 향상

--- src/main/java/com/capstone/arfly/community/repository/PostRepository.java
+++ src/main/java/com/capstone/arfly/community/repository/PostRepository.java
@@ -3,16 +3,11 @@ package com.capstone.arfly.community.repository;
 import com.capstone.arfly.community.domain.Post;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PostRepository extends JpaRepository<Post,Long> {
-    Optional<Post>findById(Long id);
 
-    @Modifying
-    @Query("UPDATE Post p SET p.like_count = :count WHERE p.id = :postId")
-    void updateLikeCount(@Param("postId") Long postId, @Param("count") int count);
+public interface PostRepository extends JpaRepository<Post, Long> {
+    Optional<Post> findById(Long id);
+
 }
--- src/main/java/com/capstone/arfly/community/util/LikeSyncScheduler.java
+++ src/main/java/com/capstone/arfly/community/util/LikeSyncScheduler.java
@@ -1,12 +1,16 @@
 package com.capstone.arfly.community.util;
 
 import com.capstone.arfly.community.repository.PostRepository;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.Cursor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ScanOptions;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -14,8 +18,8 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 @Slf4j
 public class LikeSyncScheduler {
-    private final PostRepository postRepository;
     private final RedisTemplate<String, String> redisTemplate;
+    private final JdbcTemplate jdbcTemplate;
 
     @Scheduled(
             initialDelay = 1,
@@ -23,11 +27,14 @@ public class LikeSyncScheduler {
             timeUnit = TimeUnit.MINUTES
     )
     public void updatePostLike() {
+        String sql = "UPDATE post SET like_count = ? WHERE id = ? ";
+
         log.info("Redis와 DB의 좋아요 수 동기화 작업 시작");
         ScanOptions options = ScanOptions.scanOptions()
                         .match("post:like:[0-9]*")
                                 .count(100)
                                         .build();
+        Map<Long,Integer> countUpdateMap = new HashMap<>();
         redisTemplate.executeWithStickyConnection(connection -> {
             try(Cursor<byte[]> cursor = connection.keyCommands().scan(options)){
                 while(cursor.hasNext()){
@@ -36,9 +43,12 @@ public class LikeSyncScheduler {
                     if(countStr != null){
                         Long postId = Long.parseLong(key.split(":")[2]);
                         int count = Integer.parseInt(countStr);
-                        postRepository.updateLikeCount(postId,count);
+                        countUpdateMap.put(postId,count);
                     }
                 }
+                List<Object[]> params = countUpdateMap.entrySet().stream().map(
+                        e-> new Object[]{e.getValue(),e.getKey()}).toList();
+                jdbcTemplate.batchUpdate(sql, params);
             }catch (Exception e){
                 log.error("동기화 중 오류 발생",e);
             }
