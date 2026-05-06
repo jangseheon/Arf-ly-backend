5519f8dd5515402cab5aed5151398a1f7f7d426e
Jang seheon <tpgjs3121@naver.com>
2026-05-06 16:58:37 +0900

    feat: 스마트 진단 기능 추가

--- src/main/java/com/capstone/arfly/diagnosis/controller/DiagnosisController.java
+++ src/main/java/com/capstone/arfly/diagnosis/controller/DiagnosisController.java
@@ -1,21 +1,22 @@
 package com.capstone.arfly.diagnosis.controller;
 
 import com.capstone.arfly.diagnosis.dto.DiagnosisListResponseDto;
+import com.capstone.arfly.diagnosis.dto.DiagnosisResponseDto;
 import com.capstone.arfly.diagnosis.service.DiagnosisService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/pets/diagnoses")
+@RequestMapping("/api/pets")
 public class DiagnosisController {
 
     private final DiagnosisService diagnosisService;
@@ -24,7 +25,7 @@ public class DiagnosisController {
             summary = "스마트 진단 기록 목록 조회 (무한 스크롤)",
             description = "사용자의 전체 스마트 진단 기록을 최신순으로 조회합니다. petId를 전달하면 특정 반려동물의 기록만 필터링합니다."
     )
-    @GetMapping
+    @GetMapping("/diagnoses")
     public ResponseEntity<DiagnosisListResponseDto> getDiagnosisList(
             @Parameter(description = "필터링할 반려동물 ID (입력하지 않으면 사용자의 모든 진단 기록 조회)", example = "1")
             @RequestParam(required = false) Long petId,
@@ -44,6 +45,24 @@ public class DiagnosisController {
         DiagnosisListResponseDto response = diagnosisService.getDiagnosisList(memberId, petId, cursor, size);
 
         // 3. 200 OK 응답과 함께 DTO 반환
+        return ResponseEntity.ok(response);
+    }
+
+    @Operation(
+            summary = "스마트 진단",
+            description = "사진을 전송하여 스마트 진단을 생성합니다."
+    )
+    @PostMapping(path = "/{petId}/diagnosis", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<DiagnosisResponseDto> diagnosisSkin(@Parameter(description = "반려동물 ID", example = "1")
+                                                              @RequestParam(required = false) @PathVariable Long petId,
+                                                              @Parameter(description = "진단할 사진", schema = @Schema(type = "string", format = "binary"))
+                                                              @RequestPart("file") MultipartFile file,
+                                                              @Parameter(hidden = true) @AuthenticationPrincipal UserDetails userDetails){
+
+        Long userId = Long.parseLong(userDetails.getUsername());
+
+        DiagnosisResponseDto response = diagnosisService.getDiagnosis(petId,file,userId);
+
         return ResponseEntity.ok(response);
     }
 }
